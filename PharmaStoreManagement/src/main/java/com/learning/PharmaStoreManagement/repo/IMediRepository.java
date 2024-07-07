9394462f1b67a0a667dab266fcf81eb87fbeac0c
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-07-07 23:34:23 +0530

    Delete PharmaStoreManagement directory

--- PharmaStoreManagement/src/main/java/com/learning/PharmaStoreManagement/repo/IMediRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learning.PharmaStoreManagement.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learning.PharmaStoreManagement.model.MedicalStore;
-
-public interface IMediRepository extends JpaRepository<MedicalStore, Integer> {
-
-}
